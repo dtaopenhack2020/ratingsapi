7c050be6750375a98d3498fd14614f9b1a843fde
Roland Nyamoga <rolandn1844@gmail.com>
2020-09-16 16:44:39 -0400

    do not merge with features, get rating and get ratings changes added to master

--- GetRating/index.js
+++ GetRating/index.js
@@ -1,13 +1,51 @@
 module.exports = async function (context, req) {
+
+
+    context.log(req);
+    const cosmos = require('@azure/cosmos');
+    //const endpoint = process.env.COSMOS_API_URL;
+    //Enter env variable in loadsettings.json file 
+    //COSMOS_API_URL 
+    //COSMOS_API_KEY
+    //const key = process.env.COSMOS_API_KEY;
+
+
+    const endpoint = "https://team20tr.documents.azure.com:443/";
+    const key = "GrmVMINe94HGKE7UexQOvXsDI2sAuPViM346KpwG1Pte7ajNljFav3vgMr1kB8QQsPh0Z9w9dLXkVTponxBb7g==";
+
+
+    const { CosmosClient } = cosmos;
+    var result = {};
+
+
+    const client = new CosmosClient({ endpoint, key });
+    // All function invocations also reference the same database and container.
+    const container = client.database("BFYOC").container("IcecreamRatings");
+
+
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
+    const ratingId = (req.query.ratingId || (req.body && req.body.ratingId));
+
+
+    const { resources: itemArray } = await container.items.readAll().fetchAll();
+    context.log(itemArray);
+
+
+    itemArray.forEach(element => {
+        if (element.id == ratingId) {
+            result = element;
+        }
+    });
+
+
+    if (result != null) {
+        context.res.body = JSON.stringify(result);
+        context.log(context.res.body);
+    } else{
+        context.res.body = "No records found"; 
+    }
+    context.done();
+
 }
--- GetRatings/index.js
+++ GetRatings/index.js
@@ -1,13 +1,63 @@
 module.exports = async function (context, req) {
+
+    context.log(req);
+    const cosmos = require('@azure/cosmos');
+    //const endpoint = process.env.COSMOS_API_URL;
+    //Enter env variable in loadsettings.json file 
+    //COSMOS_API_URL 
+    //COSMOS_API_KEY
+    //const key = process.env.COSMOS_API_KEY;
+    const request = require('async-request');
+    const userUrl = "https://serverlessohuser.trafficmanager.net/api/GetUser";
+
+    const endpoint = "https://team20tr.documents.azure.com:443/";
+    const key = "GrmVMINe94HGKE7UexQOvXsDI2sAuPViM346KpwG1Pte7ajNljFav3vgMr1kB8QQsPh0Z9w9dLXkVTponxBb7g==";
+
+    const { CosmosClient } = cosmos;
+    var result = {};
+
+    const client = new CosmosClient({ endpoint, key });
+    // All function invocations also reference the same database and container.
+    const container = client.database("BFYOC").container("IcecreamRatings");
+
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+    const userId = (req.query.userId || (req.body && req.body.userId));
 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
+    var user = await request(userUrl + "?userId=" + userId)
+    if (user.statusCode !== 200) {
+        context.log("User not found");
+        res.status = 404;
+        res.body = ("User not found");
+    }
+    else {
+        console.log(`Querying container: Items`);
+
+        // query to return all items
+        const querySpec = {
+            query: "SELECT * from c"
+          // query: "SELECT * from c"     
+        };
+
+        // read all items in the Items container
+        const { resources: items } = await container.items
+            .query(querySpec)
+            .fetchAll();
+
+        var itemArray = [];
+        items.forEach(item => {
+            if (item.userId==userId) {
+               itemArray.push(item);
+            }
+  //          context.log(`${item.id} - ${item.description}`);
+        });
     };
+
+    if (result != null) {
+        context.res.body = itemArray;
+        context.log(context.res.body);
+    } else {
+        context.res.body = "No records found";
+    }
+    context.done();
 }
